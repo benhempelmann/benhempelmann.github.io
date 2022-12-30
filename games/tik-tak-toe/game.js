c8e75f839ab30410a39befefa46c906f60d6a312
benhempelmann <94403398+benhempelmann@users.noreply.github.com>
2022-12-29 23:44:41 -0700

    Added tic-tac-toe

---
+++ games/tik-tak-toe/game.js
@@ -0,0 +1,59 @@
+const gameBoard = document.getElementById("game-board");
+
+let curr_player = 1;
+
+window.onclick = e => {
+    const element = e.target
+    if (e.target.tagName == "DIV"){
+        let posx = Math.round(3*(element.offsetLeft - gameBoard.offsetLeft)/gameBoard.offsetWidth);
+        let posy = Math.round(3*(element.offsetTop - gameBoard.offsetTop)/gameBoard.offsetHeight);
+        let pos = 3*posy +posx
+        if (element.innerHTML == ''){
+            if (curr_player == 1){
+                element.innerHTML = "X"; 
+                curr_player = 2;
+            }
+            else{
+                element.innerHTML = "O"; 
+                curr_player = 1;
+            }
+            if(checkWinner(pos)){
+                setTimeout(() => {
+                    if(alert('The ' + element.innerHTML + '\'s won!')){}
+                    else    window.location.reload(); 
+                  }, "300")
+                
+            };
+
+        }
+    }
+}
+
+function checkWinner(pos){
+    let boxes = gameBoard.getElementsByTagName('div');
+    newPosy = pos % 3;
+    newPosx = pos - newPosy;
+    // console.log(newPosy);
+    // console.log(newPosx);
+    // console.log(pos)
+    if(boxes[newPosy].innerHTML == boxes[newPosy+3].innerHTML && boxes[newPosy].innerHTML == boxes[newPosy+6].innerHTML){
+        return true;
+    }
+    else if(boxes[newPosx].innerHTML == boxes[newPosx+1].innerHTML && boxes[newPosx].innerHTML == boxes[newPosx+2].innerHTML){
+        return true;
+    }
+    else if(pos % 4 == 0){
+        if(boxes[0].innerHTML == boxes[4].innerHTML && boxes[0].innerHTML == boxes[8].innerHTML){
+            return true;
+        }
+    }
+    else if (pos % 2 == 0){
+        if(boxes[2].innerHTML == boxes[4].innerHTML && boxes[2].innerHTML == boxes[6].innerHTML){
+            return true;
+        }
+    }
+    else{
+        return false;
+    }
+    
+}
